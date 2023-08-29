ed0043b64e5b74484753cc142bd5f5f0b3f16551
RobertMili <robertmilicevic3869@gmail.com>
2023-08-29 14:47:05 +0200

    Trying to fix mobile on index

---
+++ artishemsida/js/fetch.js
@@ -0,0 +1,9 @@
+fetch('data/data.json')
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+    console.log("test");
--- artishemsida/js/registering.js
+++ artishemsida/js/registering.js
@@ -1,3 +1,36 @@
+const elForm = document.querySelector("#registration-form");
+const elInputFirstName = document.querySelector('#txtName');
+const elInputLastName = document.querySelector('#txtLastName');
+const elEmail = document.querySelector('#txtEmail');
+const elPassword  = document.querySelector('#txtPassword');
+const elConfirmPasswordInput = document.querySelector('#txtConfirmPassword');
+
+
+
+function confirmPassword(event) {
+
+  if (elPassword.value !== elConfirmPasswordInput.value) {
+    event.preventDefault();
+    alert("Password do not match!");
+  }
+}
+
+function inputLocalStorage(event){
+  event.preventDefault();
+
+  let firstName = elInputFirstName.value;
+  let lastName = elInputLastName.value;
+  let email = elEmail.value;
+ 
+  
+  localStorage.setItem('firstName',firstName);
+  localStorage.setItem('lastName', lastName);
+  localStorage.setItem('email',email);
+
+}
+
+
+
 const elLogin = document.querySelector("#login");
 const elMsg = document.querySelector("#feedback");
 
@@ -12,3 +45,7 @@ function checkUserName(minLength) {
     elMsg.innerHTML = "";
   }
 }
+
+elForm.addEventListener('submit', confirmPassword, false);
+elForm.addEventListener('submit', inputLocalStorage, false);
+
