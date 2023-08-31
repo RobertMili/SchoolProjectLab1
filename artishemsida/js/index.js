5c7ea90121fb98e9b465d47a24c5eb3e4e4f938d
RobertMili <robertmilicevic3869@gmail.com>
2023-08-31 16:53:06 +0200

    Shopping

--- artishemsida/js/index.js
+++ artishemsida/js/index.js
@@ -12,3 +12,53 @@ document.getElementById('add-animation').addEventListener('click', event => {
      counter.classList.add('animated-counter')
      ,1)
 })
+
+let count1 = 0;
+
+const counter1 = document.getElementById('counter');
+document.getElementById('add-animation1').addEventListener('click', event => {
+    const cl = counter.classList;
+    const c = 'animated-counter';
+    count++;
+
+    counter.innerText = count;
+    cl.remove(c, cl.contains(c));
+    setTimeout(() =>
+     counter.classList.add('animated-counter')
+     ,1)
+})
+
+let count2 = 0;
+
+const counter2 = document.getElementById('counter');
+document.getElementById('add-animation2').addEventListener('click', event => {
+    const cl = counter.classList;
+    const c = 'animated-counter';
+    count++;
+
+    counter.innerText = count;
+    cl.remove(c, cl.contains(c));
+    setTimeout(() =>
+     counter.classList.add('animated-counter')
+     ,1)
+})
+
+
+let count3 = 0;
+
+const counter3 = document.getElementById('counter');
+document.getElementById('add-animation3').addEventListener('click', event => {
+    const cl = counter.classList;
+    const c = 'animated-counter';
+    count++;
+
+    counter.innerText = count;
+    cl.remove(c, cl.contains(c));
+    setTimeout(() =>
+     counter.classList.add('animated-counter')
+     ,1)
+})
+
+
+
+
